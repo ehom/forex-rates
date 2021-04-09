e956a5733b9180edc53e28c249e3aa136df66c44
ehom <ehom@acm.org>
2021-04-09 01:55:43 -0700

    use finnhub data as source of usd exchange rates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -6,22 +8,39 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-document.title = "USD forex rates";
+var currencyCodes = ["USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "HKD", "NZD", "SEK", "KRW", "SGD", "NOK", "MXN", "INR", "RUB", "ZAR", "TRY", "BRL", "TWD", "DKK", "PLN", "THB", "IDR", "HUF", "CZK", "ILS", "CLP", "PHP", "AED", "COP", "SAR", "MYR", "RON"];
 
 var App = function (_React$Component) {
   _inherits(App, _React$Component);
 
-  function App(props) {
+  function App() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
     _classCallCheck(this, App);
 
-    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
 
-    console.debug("ctor");
-    _this.state = {
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = App.__proto__ || Object.getPrototypeOf(App)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
       date: undefined,
-      rates: {}
-    };
-    return _this;
+      rates: [],
+      language: navigator.language
+    }, _this.fetchedRates = [], _this.handleLanguageChange = function (event) {
+      console.debug("handleLanguageChange:", event);
+
+      var rates = Helper.createCurrencyList(currencyCodes, _this.fetchedRates);
+
+      console.debug("REcreated table of formatted rates:", rates);
+
+      _this.setState({
+        date: _this.state.date,
+        rates: rates,
+        language: navigator.language
+      });
+    }, _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(App, [{
@@ -31,18 +50,24 @@ var App = function (_React$Component) {
 
       console.debug("componentDidMount");
 
-      var EXCHANGE_RATES = 'https://raw.githubusercontent.com/ehom/external-data/master/exchangeratesapi/forex-rates.json';
+      window.addEventListener("languagechange", this.handleLanguageChange.bind(this));
+
+      var EXCHANGE_RATES = "https://raw.githubusercontent.com/ehom/external-data/master/finnhub/usd.json";
 
       fetch(EXCHANGE_RATES).then(function (response) {
         return response.json();
       }).then(function (json) {
-        console.debug(json);
-        return json;
-      }).then(function (json) {
-        delete json.rates['USD'];
+        console.debug("rates fetched:", json.quote);
+
+        _this2.fetchedRates = json.quote;
+
+        var rates = Helper.createCurrencyList(currencyCodes, json.quote);
+
+        console.debug("created table of formatted rates:", rates);
+
         _this2.setState({
-          date: json.date,
-          rates: json.rates
+          date: new Date(json.timeStamp),
+          rates: rates
         });
       }).catch(function (error) {
         return console.log(error);
@@ -51,40 +76,54 @@ var App = function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      console.debug("render");
+      console.debug("render: table of formatted rates:", this.state.rates);
 
-      if (this.state.date !== undefined) {
-        return React.createElement(
-          React.Fragment,
-          null,
-          React.createElement(
-            "div",
-            { className: "jumbotron pt-4 pb-4" },
-            React.createElement(
-              "h3",
-              { className: "h3" },
-              "How much is 1 US Dollar worth today?"
-            )
-          ),
-          React.createElement(Motd, { date: this.state.date }),
-          React.createElement(
-            "div",
-            { "class": "row" },
-            React.createElement(Rates, { rates: this.state.rates })
-          )
-        );
-      }
+      var tableRows = this.state.rates.map(function (entry) {
+        return React.createElement(TableRow, { entry: entry, key: entry.code });
+      });
 
       return React.createElement(
-        React.Fragment,
-        null,
+        "div",
+        { className: "container" },
         React.createElement(
           "div",
-          { className: "jumbotron pt-4 pb-4" },
+          { className: "jumbotron pt-3 pb-3 mb-3" },
           React.createElement(
-            "h3",
-            { className: "h3" },
-            "How much is 1 US Dollar worth today?"
+            "div",
+            { className: "row" },
+            React.createElement(
+              "div",
+              { className: "col" },
+              React.createElement(
+                "h3",
+                null,
+                "USD EXCHANGE RATES"
+              )
+            ),
+            React.createElement(
+              "div",
+              { className: "col text-right" },
+              React.createElement(Helper.FormattedDate, {
+                locale: this.state.language,
+                date: this.state.date
+              })
+            )
+          )
+        ),
+        React.createElement(Instructions, null),
+        React.createElement(
+          "div",
+          null,
+          React.createElement(
+            "table",
+            { className: "table table-hover table-dark" },
+            React.createElement(
+              "tbody",
+              null,
+              React.createElement(BlankRow, null),
+              tableRows,
+              React.createElement(BlankRow, null)
+            )
           )
         )
       );
@@ -94,47 +133,121 @@ var App = function (_React$Component) {
   return App;
 }(React.Component);
 
-var Rates = function Rates(_ref) {
-  var rates = _ref.rates;
-
-  var currencyCodes = function currencyCodes(object) {
-    var currencies = Object.keys(object);
-    currencies.sort();
-    return currencies;
+var Instructions = function Instructions() {
+  var handleClick = function handleClick(event) {
+    // dismiss the alert by closing the parent of the button.
+    event.target.parentElement.parentElement.style.display = "none";
   };
+  var THINKING_FACE = "\uD83E\uDD14";
+  var SLIGHTLY_SMILING = "\uD83D\uDE42";
 
-  var formatted = currencyCodes(rates).map(function (code) {
-    return React.createElement(
-      "div",
-      { className: "col-sm-4" },
+  var styling = { fontSize: "14pt" };
+  return React.createElement(
+    "div",
+    {
+      className: "alert alert-light alert-dismissible fade show",
+      role: "alert",
+      style: styling
+    },
+    React.createElement(
+      "button",
+      { type: "button", className: "close", onClick: handleClick },
       React.createElement(
-        "table",
-        { className: "table table-hover" },
-        React.createElement(
-          "tbody",
-          null,
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "td",
-              { className: "pr-4" },
-              React.createElement(CurrencyFormat, { locale: "en", displayType: "name", currencyCode: code, value: rates[code] })
-            )
-          ),
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "td",
-              { className: "pr-5" },
-              React.createElement(CurrencyFormat, { locale: "en", displayType: "code", currencyCode: code, value: rates[code] })
-            )
-          )
-        )
+        "span",
+        null,
+        "\xD7"
       )
+    ),
+    " ",
+    React.createElement(
+      "strong",
+      null,
+      "Note:"
+    ),
+    " The rate table is clickable ",
+    THINKING_FACE,
+    " Also, the page will update if you change the UI language of your browser",
+    " ",
+    SLIGHTLY_SMILING
+  );
+};
+
+var BlankRow = function BlankRow() {
+  return React.createElement(
+    "tr",
+    null,
+    React.createElement("td", null),
+    React.createElement("td", null)
+  );
+};
+
+var TableRow = function TableRow(_ref2) {
+  var entry = _ref2.entry;
+
+  console.debug("entry:", entry);
+
+  var _React$useState = React.useState(0),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      formatIndex = _React$useState2[0],
+      setFormatIndex = _React$useState2[1];
+
+  var parts = entry.formats[formatIndex];
+
+  console.debug("parts:", parts);
+
+  var formatted = parts.map(function (part) {
+    var key = part.id + " " + part.value;
+    if (part.type !== "currency") {
+      return React.createElement(
+        "span",
+        { className: "app-rate", key: key },
+        part.value
+      );
+    }
+    return React.createElement(
+      React.Fragment,
+      { key: key },
+      part.value
     );
   });
+
+  var handleClick = function handleClick(event) {
+    console.debug("Row clicked", event);
+    setFormatIndex(formatIndex + 1 >= entry.formats.length ? 0 : formatIndex + 1);
+  };
+
+  return React.createElement(
+    "tr",
+    { onClick: handleClick, key: entry.code },
+    React.createElement(
+      "td",
+      { className: "text-right app-flag" },
+      entry.flag
+    ),
+    React.createElement(
+      "td",
+      { className: "text-center" },
+      formatted
+    )
+  );
+};
+
+var Helper = Helper || {};
+
+Helper.FormattedDate = function (_ref3) {
+  var locale = _ref3.locale,
+      date = _ref3.date;
+
+  if (date === undefined) {
+    return React.createElement(React.Fragment, null);
+  }
+
+  var formatted = new Intl.DateTimeFormat(locale, {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  }).format(date);
 
   return React.createElement(
     React.Fragment,
@@ -143,4 +256,84 @@ var Rates = function Rates(_ref) {
   );
 };
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('parent'));
+Helper.FormattedDate.defaultProps = {
+  locale: navigator.language
+};
+
+var FLAGS = {
+  USD: "\uD83C\uDDFA\uD83C\uDDF8",
+  AUD: "\uD83C\uDDE6\uD83C\uDDFA",
+  EUR: "\uD83C\uDDEA\uD83C\uDDFA",
+  BGN: "\uD83C\uDDE7\uD83C\uDDEC",
+  BRL: "\uD83C\uDDE7\uD83C\uDDF7",
+  CAD: "\uD83C\uDDE8\uD83C\uDDE6",
+  CHF: "\uD83C\uDDE8\uD83C\uDDED",
+  CNY: "\uD83C\uDDE8\uD83C\uDDF3",
+  CZK: "\uD83C\uDDE8\uD83C\uDDFF",
+  DKK: "\uD83C\uDDE9\uD83C\uDDF0",
+  GBP: "\uD83C\uDDEC\uD83C\uDDE7",
+  HKD: "\uD83C\uDDED\uD83C\uDDF0",
+  HRK: "\uD83C\uDDED\uD83C\uDDF7",
+  HUF: "\uD83C\uDDED\uD83C\uDDFA",
+  IDR: "\uD83C\uDDEE\uD83C\uDDE9",
+  ILS: "\uD83C\uDDEE\uD83C\uDDF1",
+  INR: "\uD83C\uDDEE\uD83C\uDDF3",
+  ISK: "\uD83C\uDDEE\uD83C\uDDF8",
+  JPY: "\uD83C\uDDEF\uD83C\uDDF5",
+  KRW: "\uD83C\uDDF0\uD83C\uDDF7",
+  MXN: "\uD83C\uDDF2\uD83C\uDDFD",
+  MYR: "\uD83C\uDDF2\uD83C\uDDFE",
+  NOK: "\uD83C\uDDF3\uD83C\uDDF4",
+  NZD: "\uD83C\uDDF3\uD83C\uDDFF",
+  PHP: "\uD83C\uDDF5\uD83C\uDDED",
+  PLN: "\uD83C\uDDF5\uD83C\uDDF1",
+  RON: "\uD83C\uDDF7\uD83C\uDDF4",
+  RUB: "\uD83C\uDDF7\uD83C\uDDFA",
+  SEK: "\uD83C\uDDF8\uD83C\uDDEA",
+  SGD: "\uD83C\uDDF8\uD83C\uDDEC",
+  THB: "\uD83C\uDDF9\uD83C\uDDED",
+  TRY: "\uD83C\uDDF9\uD83C\uDDF7",
+  ZAR: "\uD83C\uDDFF\uD83C\uDDE6",
+  COP: "\uD83C\uDDE8\uD83C\uDDF4",
+  CLP: "\uD83C\uDDE8\uD83C\uDDF1",
+  TWD: "\uD83C\uDDF9\uD83C\uDDFC",
+  AED: "\uD83C\uDDE6\uD83C\uDDEA",
+  SAR: "\uD83C\uDDF8\uD83C\uDDE6"
+};
+
+Helper.getFlag = function (code) {
+  return code in FLAGS ? FLAGS[code] : FLAGS["EUR"];
+};
+
+Helper.createFormatter = function (code, currencyDisplay) {
+  return new Intl.NumberFormat(navigator.language, {
+    style: "currency",
+    currency: code,
+    currencyDisplay: currencyDisplay,
+    minimumFractionDigits: 2
+  });
+};
+
+Helper.createCurrencyList = function (codes, rates) {
+  var currencies = codes.map(function (code) {
+    var rate = rates[code];
+    var codeFormatter = Helper.createFormatter(code, "code");
+    var symbolFormatter = Helper.createFormatter(code, "symbol");
+
+    var formats = [Helper.createFormatter(code, "name").formatToParts(rate), codeFormatter.formatToParts(rate)];
+
+    if (codeFormatter.format(rate) !== symbolFormatter.format(rate)) {
+      formats.push(symbolFormatter.formatToParts(rate));
+    }
+
+    return {
+      code: code,
+      flag: Helper.getFlag(code),
+      formats: formats
+    };
+  }, []);
+
+  return currencies;
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,153 +1,295 @@
-document.title = "USD forex rates";
+const currencyCodes = [
+  "USD",
+  "EUR",
+  "JPY",
+  "GBP",
+  "AUD",
+  "CAD",
+  "CHF",
+  "CNY",
+  "HKD",
+  "NZD",
+  "SEK",
+  "KRW",
+  "SGD",
+  "NOK",
+  "MXN",
+  "INR",
+  "RUB",
+  "ZAR",
+  "TRY",
+  "BRL",
+  "TWD",
+  "DKK",
+  "PLN",
+  "THB",
+  "IDR",
+  "HUF",
+  "CZK",
+  "ILS",
+  "CLP",
+  "PHP",
+  "AED",
+  "COP",
+  "SAR",
+  "MYR",
+  "RON"
+];
 
 class App extends React.Component {
   state = {
     date: undefined,
-    rates: {},
-    cardView: true,
-    listView: false
-  }
+    rates: [],
+    language: navigator.language
+  };
+
+  fetchedRates = [];
 
   componentDidMount() {
     console.debug("componentDidMount");
 
-    const EXCHANGE_RATES = 'https://raw.githubusercontent.com/ehom/external-data/master/exchangeratesapi/forex-rates.json';
+    window.addEventListener(
+      "languagechange",
+      this.handleLanguageChange.bind(this)
+    );
+
+    const EXCHANGE_RATES =
+      "https://raw.githubusercontent.com/ehom/external-data/master/finnhub/usd.json";
 
     fetch(EXCHANGE_RATES)
-      .then(response => response.json())
-      .then(
-        json => {
-          console.debug(json);
-          return json;
-        })
-      .then(
-        json => {
-          delete json.rates['USD'];
+      .then((response) => response.json())
+      .then((json) => {
+        console.debug("rates fetched:", json.quote);
 
-          const [year, month, day] = json.date.split('-');
-          const date = new Date(Date.UTC(year, month - 1, day, 8, 0, 0));
-          const localDate = new Intl.DateTimeFormat(navigator.language, {
-            weekday: "short", year: "numeric", month: "short", day: "numeric"}).format(date);
+        this.fetchedRates = json.quote;
 
-          this.setState({
-            date: localDate,
-            rates: json.rates
-          });
-        })
-      .catch(error => console.log(error));
+        const rates = Helper.createCurrencyList(currencyCodes, json.quote);
+
+        console.debug("created table of formatted rates:", rates);
+
+        this.setState({
+          date: new Date(json.timeStamp),
+          rates: rates
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
-  clickCardView(event) {
-    console.debug("click Card View:", event);
+  handleLanguageChange = (event) => {
+    console.debug("handleLanguageChange:", event);
+
+    const rates = Helper.createCurrencyList(currencyCodes, this.fetchedRates);
+
+    console.debug("REcreated table of formatted rates:", rates);
 
     this.setState({
-      cardView: true,
-      listView: false
+      date: this.state.date,
+      rates: rates,
+      language: navigator.language
     });
-  }
-
-  clickListView(event) {
-    console.debug("click List View", event);
-
-    this.setState({
-      cardView: false,
-      listView: true
-    });
-  }
-
-  render() {
-    console.debug("render app");
-
-    let rates = "";
-    const isReady = this.state.date !== undefined;
-
-    if (isReady) {
-      if (this.state.listView) {
-        rates = (
-          <React.Fragment>
-            <Motd date={this.state.date} />
-            <ListView rates={this.state.rates} />
-          </React.Fragment>
-        );
-      } else {
-        rates = (
-          <React.Fragment>
-            <Motd date={this.state.date} />
-            <CardView rates={this.state.rates} />
-          </React.Fragment>
-        );
-      }
-    }
-
-    return (
-      <React.Fragment>
-        <div className="jumbotron pt-4 pb-4 mb-2">
-          <h3 className="h3">How much is 1 US Dollar worth today?</h3>
-        </div>
-        <div className="container border mb-3">
-          <div className="form-check form-check-inline">
-            <RadioButton label="Card View" checked={this.state.cardView} onClick={this.clickCardView.bind(this)} />
-          </div>
-          <div className="form-check form-check-inline">
-            <RadioButton label="List View" checked={this.state.listView} onClick={this.clickListView.bind(this)} />
-          </div>
-        </div>
-        <div>
-        {rates}
-        </div>
-      </React.Fragment>
-    );
-  }  
-}
-
-const RadioButton = ({label, checked, onClick}) => {
-  return (
-    <label className="form-check-label">
-      <input className="form-check-input" type="radio" checked={checked} onClick={onClick} />
-      {label}
-    </label>
-  );
-};
-
-const ListView = ({rates}) => {
-  const currencyCodes = (object) => {
-    let currencies = Object.keys(object);
-    currencies.sort();
-    return currencies;
   };
 
-  const tableRows = currencyCodes(rates).map((code) => {
+  render() {
+    console.debug("render: table of formatted rates:", this.state.rates);
+
+    const tableRows = this.state.rates.map((entry) => (
+      <TableRow entry={entry} key={entry.code} />
+    ));
+
     return (
-      <tr>
-        <td>
-          <CurrencyFormat locale='en' displayType='name' currencyCode={code} value={rates[code]}/>
-        </td>
-        <td>
-          <CurrencyFormat locale='en' displayType='code' currencyCode={code} value={rates[code]}/>
-        </td>
-        <td>
-          <CurrencyFormat locale='en' displayType='symbol' currencyCode={code} value={rates[code]}/>
-        </td>
-      </tr>
+      <div className="container">
+        <div className="jumbotron pt-3 pb-3 mb-3">
+          <div className="row">
+            <div className="col">
+              <h3>USD EXCHANGE RATES</h3>
+            </div>
+            <div className="col text-right">
+              <Helper.FormattedDate
+                locale={this.state.language}
+                date={this.state.date}
+              />
+            </div>
+          </div>
+        </div>
+        <Instructions />
+        <div>
+          <table className="table table-hover table-dark">
+            <tbody>
+              <BlankRow />
+              {tableRows}
+              <BlankRow />
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
-  });
+  }
+}
 
-  const style = {"text-align": "right"};
+const Instructions = () => {
+  const handleClick = (event) => {
+    // dismiss the alert by closing the parent of the button.
+    event.target.parentElement.parentElement.style.display = "none";
+  };
+  const THINKING_FACE = "\uD83E\uDD14";
+  const SLIGHTLY_SMILING = "\ud83d\ude42";
 
+  const styling = { fontSize: "14pt" };
   return (
-    <React.Fragment>
-      <table className='table table-sm table-responsive-sm table-hover table-striped'>
-        <thead>
-          <tr>
-            <th style={style}>currency name</th>
-            <th style={style}>code</th>
-            <th style={style}>symbol</th>
-          </tr>
-        </thead>
-        <tbody>{tableRows}</tbody>
-      </table>
-    </React.Fragment>
+    <div
+      className="alert alert-light alert-dismissible fade show"
+      role="alert"
+      style={styling}
+    >
+      <button type="button" className="close" onClick={handleClick}>
+        <span>×</span>
+      </button>{" "}
+      <strong>Note:</strong> The rate table is clickable {THINKING_FACE} Also,
+      the page will update if you change the UI language of your browser{" "}
+      {SLIGHTLY_SMILING}
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('parent'));
+const BlankRow = () => {
+  return (
+    <tr>
+      <td />
+      <td />
+    </tr>
+  );
+};
+
+const TableRow = ({ entry }) => {
+  console.debug("entry:", entry);
+
+  const [formatIndex, setFormatIndex] = React.useState(0);
+
+  const parts = entry.formats[formatIndex];
+
+  console.debug("parts:", parts);
+
+  const formatted = parts.map((part) => {
+    const key = part.id + " " + part.value;
+    if (part.type !== "currency") {
+      return <span className="app-rate" key={key}>{part.value}</span>;
+    }
+    return <React.Fragment key={key}>{part.value}</React.Fragment>;
+  });
+
+  const handleClick = (event) => {
+    console.debug("Row clicked", event);
+    setFormatIndex(
+      formatIndex + 1 >= entry.formats.length ? 0 : formatIndex + 1
+    );
+  };
+
+  return (
+    <tr onClick={handleClick} key={entry.code}>
+      <td className="text-right app-flag">{entry.flag}</td>
+      <td className="text-center">{formatted}</td>
+    </tr>
+  );
+};
+
+var Helper = Helper || {};
+
+Helper.FormattedDate = ({ locale, date }) => {
+  if (date === undefined) {
+    return <React.Fragment></React.Fragment>;
+  }
+
+  const formatted = new Intl.DateTimeFormat(locale, {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  }).format(date);
+
+  return <React.Fragment>{formatted}</React.Fragment>;
+};
+
+Helper.FormattedDate.defaultProps = {
+  locale: navigator.language
+};
+
+const FLAGS = {
+  USD: "\ud83c\uddfa\ud83c\uddf8",
+  AUD: "\ud83c\udde6\ud83c\uddfa",
+  EUR: "\ud83c\uddea\ud83c\uddfa",
+  BGN: "\ud83c\uDDE7\ud83c\uDDEC",
+  BRL: "\ud83c\uDDE7\ud83c\uDDF7",
+  CAD: "\ud83c\udde8\ud83c\udde6",
+  CHF: "\ud83c\uDDE8\ud83c\uDDED",
+  CNY: "\ud83c\uDDE8\ud83c\uDDF3",
+  CZK: "\ud83c\uDDE8\ud83c\uDDFF",
+  DKK: "\ud83c\udde9\ud83c\uddf0",
+  GBP: "\ud83c\uDDEC\ud83c\uDDE7",
+  HKD: "\ud83c\uDDED\ud83c\uDDF0",
+  HRK: "\ud83c\uDDED\ud83c\uDDF7",
+  HUF: "\ud83c\uDDED\ud83c\uDDFA",
+  IDR: "\ud83c\uDDEE\ud83c\uDDE9",
+  ILS: "\ud83c\uDDEE\ud83c\uDDF1",
+  INR: "\ud83c\uDDEE\ud83c\uDDF3",
+  ISK: "\ud83c\uDDEE\ud83c\uDDF8",
+  JPY: "\ud83c\uDDEF\ud83c\uDDF5",
+  KRW: "\ud83c\uDDF0\ud83c\uDDF7",
+  MXN: "\ud83c\uDDF2\ud83c\uDDFD",
+  MYR: "\ud83c\uDDF2\ud83c\uDDFE",
+  NOK: "\ud83c\uDDF3\ud83c\uDDF4",
+  NZD: "\ud83c\uDDF3\ud83c\uDDFF",
+  PHP: "\ud83c\uDDF5\ud83c\uDDED",
+  PLN: "\ud83c\uDDF5\ud83c\uDDF1",
+  RON: "\ud83c\uDDF7\ud83c\uDDF4",
+  RUB: "\ud83c\uDDF7\ud83c\uDDFA",
+  SEK: "\ud83c\uDDF8\ud83c\uDDEA",
+  SGD: "\ud83c\uDDF8\ud83c\uDDEC",
+  THB: "\ud83c\uDDF9\ud83c\uDDED",
+  TRY: "\ud83c\uDDF9\ud83c\uDDF7",
+  ZAR: "\ud83c\uDDFF\ud83c\uDDE6",
+  COP: "\ud83c\uDDE8\ud83c\uDDF4",
+  CLP: "\ud83c\uDDE8\ud83c\uDDF1",
+  TWD: "\ud83c\uDDF9\ud83c\uDDFC",
+  AED: "\ud83c\uDDE6\ud83c\uDDEA",
+  SAR: "\ud83c\uDDF8\ud83c\uDDE6"
+};
+
+Helper.getFlag = (code) => {
+  return (code in FLAGS) ? FLAGS[code] : FLAGS["EUR"];
+};
+
+Helper.createFormatter = (code, currencyDisplay) => {
+  return new Intl.NumberFormat(navigator.language, {
+    style: "currency",
+    currency: code,
+    currencyDisplay: currencyDisplay,
+    minimumFractionDigits: 2
+  });
+};
+
+Helper.createCurrencyList = (codes, rates) => {
+  let currencies = codes.map((code) => {
+    const rate = rates[code];
+    const codeFormatter = Helper.createFormatter(code, "code");
+    const symbolFormatter = Helper.createFormatter(code, "symbol");
+
+    let formats = [
+      Helper.createFormatter(code, "name").formatToParts(rate),
+      codeFormatter.formatToParts(rate)
+    ];
+
+    if (codeFormatter.format(rate) !== symbolFormatter.format(rate)) {
+      formats.push(symbolFormatter.formatToParts(rate));
+    }
+
+    return {
+      code: code,
+      flag: Helper.getFlag(code),
+      formats: formats
+    };
+  }, []);
+
+  return currencies;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
